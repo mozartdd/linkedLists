27ed32d87d46ac18b0db4dad7f9605e7ce06b5fd
Dainis Dilevka <thedenisovan@proton.me>
2025-07-05 15:10:07 +0100

    Added function to append node to end of linked list

--- linkedList.js
+++ linkedList.js
@@ -1,12 +1,33 @@
 class LinkedList {
   constructor() {
-
+    this.size = 0;
+    this.head = null;
+    this.tail = null;
+    // Class to hold each individual node element
+    this.node = class Node {
+      constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+      }
+    }
+  }
+  // Adds node to end of linked list
+  append(value) {
+    let node = new this.node(value);
+    if (this.size === 0) { // If size is 0 node is head and tail at same time
+      this.head = node;
+    } else {
+      this.tail.nextNode = node;
+    }
+    this.size++;
+    this.tail = node;
   }
 }
 
-class Node {
-  constructor() {
-    this.value = null;
-    this.nextNode = null;
-  }
-}
+
+const li = new LinkedList();
+li.append('dog');
+li.append('cat');
+
+console.log(li);
+
